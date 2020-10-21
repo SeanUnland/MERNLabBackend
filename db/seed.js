dd87598fa284261e7ad51a913f0f0083cf224f78
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-20 22:02:13 -0400

    removed seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,19 @@
-const mongoose = require("./connection");
-const Owner = require("../models/Owner");
-const Cat = require("../models/Cat");
+// const mongoose = require("./connection");
+// const Owner = require("../models/Owner");
+// const Cat = require("../models/Cat");
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-Cat.deleteMany({}).then(() => {
-  Cat.insertMany(manyCats).then((cats) => {
-    console.log(`cats`, cats);
-    db.close();
-  });
-});
+// Cat.deleteMany({}).then(() => {
+//   Cat.insertMany(manyCats).then((cats) => {
+//     console.log(`cats`, cats);
+//     db.close();
+//   });
+// });
 
-Owner.deleteMany({}).then(() => {
-  Owner.insertMany(manyOwners).then((owners) => {
-    console.log(`owners`, owners);
-    db.close();
-  });
-});
+// Owner.deleteMany({}).then(() => {
+//   Owner.insertMany(manyOwners).then((owners) => {
+//     console.log(`owners`, owners);
+//     db.close();
+//   });
+// });
