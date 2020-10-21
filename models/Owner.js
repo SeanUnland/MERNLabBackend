32993989e19f9cbf7dec1ee0d1ac3fccaef0bd8a
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-21 10:28:19 -0400

    updated backend models

--- models/Owner.js
+++ models/Owner.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const ownerSchema = new Schema({
   name: String,
   age: Number,
+  cats: [{ ref: "Cat", type: mongoose.Types.ObjectId }],
 });
 
 const Owner = mongoose.model("Owner", ownerSchema);
